28212ed62b680c88fb2224915dbd0b02fc6f60c3
lweiwei020418-png <lweiwei020418@gmail.com>
2026-02-08 23:28:20 +0800

    Update WishPage.tsx

--- components/WishPage.tsx
+++ components/WishPage.tsx
@@ -165,7 +165,7 @@ const WishPage: React.FC<WishPageProps> = ({ config, onWishSubmit, onNext }) => 
               >
                 <div className="absolute inset-0 z-0 pointer-events-auto">
                    <img 
-                     src={`${t.characterImg}&v=${Date.now()}`} 
+                     src={t.characterImg} 
                      alt="Card" 
                      className="w-full h-full object-cover"
                    />
