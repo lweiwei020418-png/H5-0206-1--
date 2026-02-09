8791c149a25557765c592ff67e96ef37ba298825
lweiwei020418-png <lweiwei020418@gmail.com>
2026-02-09 18:13:30 +0800

    Update PrivacyModal.tsx

--- components/PrivacyModal.tsx
+++ components/PrivacyModal.tsx
@@ -16,12 +16,8 @@ const PrivacyModal: React.FC<PrivacyModalProps> = ({ onAgree }) => {
           <p className="font-bold text-yellow-200">【信息收集必要性】</p>
           <p>为了精准为您发放活动奖励（如复试礼包、奖学金等），我们需要收集您的昵称及联系方式。您的隐私安全受到有道考研的严格保护，仅用于本次活动及后续教务沟通。</p>
           
-          <p className="font-bold text-yellow-200">【报喜活动大奖】</p>
-          <ul className="list-decimal list-inside space-y-1">
-            <li><span className="text-yellow-400">一等奖：</span>录屏被选中进入宣传片，享万元奖学金池分红或全额免单。</li>
-            <li><span className="text-yellow-400">二等奖：</span>上传有效成绩截图，立得《2026复试面试真题库》。</li>
-            <li><span className="text-yellow-400">三等奖：</span>发布心愿卡并分享，得10元考研课程优惠券。</li>
-          </ul>
+          <p className="font-bold text-yellow-200">【社群幸运抽奖】</p>
+          <p>加入社群，即可解锁有道词典笔等惊喜大奖！活动期间还有多轮快闪福利突袭，所有惊喜均将通过社群抽奖进行派送，快来加入社群，抢占幸运名额！</p>
         </div>
 
         <button 
